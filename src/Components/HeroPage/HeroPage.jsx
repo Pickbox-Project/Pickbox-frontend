0effee8f69b339b30ccb236b11b8de4bb084df21
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-07 12:38:55 +0100

    Started working on the HeroPage Component

--- src/Components/HeroPage/HeroPage.jsx
+++ src/Components/HeroPage/HeroPage.jsx
@@ -4,7 +4,14 @@ import "./HeroPage.css"
 const HeroPage = () => {
   return (
     <div className='heropage-container'>
-        
+        <div className='heropage-details'>
+            <div className='heropage-text'>
+                <h2>From your <span> STORE </span> to their <br/>
+                <span>DOORSTEPS, </span> we will <br/>
+                handle the delivery.               
+                </h2>
+            </div>
+        </div>
     </div>
   )
 }
