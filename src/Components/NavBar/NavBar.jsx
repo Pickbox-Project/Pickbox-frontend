543bba9077ddac115ac10911e2a43d0a78151fbe
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-07 10:48:33 +0100

    Started working on the Navbar component as well as adding some Assets required

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./NavBar.css"
+import pickboxLogo from "../../Assets/images/pickbox-logo.png"
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='navbar-container'>
+        <div className='pickbox-logo'>
+            <img src={pickboxLogo} alt="Pickbox Logo" />
+        </div>
+    </div>
   )
 }
 
