8783d2b5c98de13d5ccb3a3b3a07c0a9f3b17879
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-07 11:33:31 +0100

    Started working on the Navbar component as well as adding some Assets required

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div>
+      <Router>
       <NavBar/>
+        <Routes>
+          
+        </Routes>
+      </Router>
     </div>
   );
 }
