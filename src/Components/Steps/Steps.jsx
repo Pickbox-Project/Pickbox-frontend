7e9e3632e41721b168299eb4c445c3014982fb20
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-09 11:27:57 +0100

    Started working on the Steps Component

--- src/Components/Steps/Steps.jsx
+++ src/Components/Steps/Steps.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-import "./Steps.css"
+import "./Steps.css";
+import stepsVector from "../../Assets/images/steps-vector.png";
 
 const Steps = () => {
-  return <div>Steps</div>;
+  return (
+    <div className="steps-container">
+      <div className="steps-text">
+        <h4>
+          <span>3</span> easy steps of delivery
+        </h4>
+        <img src={stepsVector} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Steps;
