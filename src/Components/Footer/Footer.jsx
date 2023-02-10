f345a810c2a2edf0168783629de99d08090b14fb
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-10 16:45:53 +0100

    Added Footer to the App.js

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-  return <div className="footer-container">
-        
-  </div>;
+  return (
+    <div className="footer-container">
+      <div className="footer-details">
+        <div className="footer-logo-text"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
