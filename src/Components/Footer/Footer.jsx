c2516bd58c33133f7001b5c693f8b783b3125ffe
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-10 16:49:25 +0100

    Started working on the Footer Component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "./Footer.css";
+import pickBoxLogo from "../../Assets/images/pickbox-logo.png"
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer-details">
-        <div className="footer-logo-text"></div>
+        <div className="footer-logo-text">
+            <img src={pickBoxLogo} alt="Pickbox Logo" />
+            <p>Pickbox logistics is eradicating logistics hassle by providing swift and affordable market to door-step delivery services.</p>
+        </div>
+        <div className="footer-navigation">
+            <h5 className="footer-title">Navigation</h5>
+            <ul className="navigation-links">
+                <li></li>
+            </ul>
+        </div>
       </div>
     </div>
   );
