01d803504a26f9a4fc3c73bce5893e05a6895551
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-13 16:20:45 +0100

    Started working on Contact Us Component and Imported CSS

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./ContactUs.css";
 const ContactUs = () => {
-  return <div className="contact-us-container"></div>;
+  return (
+    <div className="contact-us-container">
+      <h5 className="contact-us-text">Contact Us</h5>
+    </div>
+  );
 };
 
 export default ContactUs;
