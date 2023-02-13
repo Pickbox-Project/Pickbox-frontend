954856bc63e162eb781fc4d99a2b073780615e19
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-13 15:36:33 +0100

    Started working on the Frequently Asked Question

--- src/Components/Faqs/Faqs.jsx
+++ src/Components/Faqs/Faqs.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
-import "./Faqs.css"
+import React from "react";
+import { Menu } from "antd";
+import "./Faqs.css";
+import { useState } from "react";
 
-const Faqs = () => {
-  return (
-    <div className='faqs-contact-container'>
-        <div className='faqs-container'>
-            <h5 className='faqs-text'>Frequently asked question</h5>
-        </div>
-    </div>
-  )
+function getItem(label, key, children, type) {
+  return {
+    key,
+    children,
+    label,
+    type,
+  };
 }
+const items = [
+  getItem("How do I...", "sub1", [getItem("Option", "1")]),
+  getItem("How do I...", "sub2", [getItem("Option", "2")]),
+];
+const rootSubmenuKeys = ["sub1", "sub2"];
+const Faqs = () => {
+  const [openKeys, setOpenKeys] = useState(["sub1"]);
+  const onOpenChange = (keys) => {
+    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+    if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+      setOpenKeys(keys);
+    } else {
+      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+    }
+  };
+  return (
+    <div className="faqs-contact-container">
+      <div className="faqs-container">
+        <h5 className="faqs-text">Frequently asked question</h5>
+        <Menu
+          mode="inline"
+          openKeys={openKeys}
+          onOpenChange={onOpenChange}
+          style={{ width: "100%" }}
+          items={items}
+        ></Menu>
+      </div>
+    </div>
+  );
+};
 
-export default Faqs
+export default Faqs;
