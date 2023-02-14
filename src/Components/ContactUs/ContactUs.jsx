da3bd2717541b5c2c425751ecf40b7e4a8a8491d
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-14 11:05:43 +0100

    Started working on Contact Us Component and Imported CSS

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -4,7 +4,7 @@ const ContactUs = () => {
   return (
     <div className="contact-us-container">
       <h5 className="contact-us-text">Contact Us</h5>
-      <form>
+      <form className="contact-form">
         <label htmlFor="name-input">Name</label>
         <input type="text" name="" id="name-input" placeholder="Write your name"/>
       </form>
