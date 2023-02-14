ac0f6ea23a9a19570e5f4ecfe8c86d8c81afb1b8
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-14 11:03:46 +0100

    Started working on Contact Us Component and Imported CSS

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -4,6 +4,10 @@ const ContactUs = () => {
   return (
     <div className="contact-us-container">
       <h5 className="contact-us-text">Contact Us</h5>
+      <form>
+        <label>Name</label>
+        <input type="text" name="" id="" placeholder="Write your name"/>
+      </form>
     </div>
   );
 };
