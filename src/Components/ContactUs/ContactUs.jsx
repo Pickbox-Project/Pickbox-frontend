7df2eff5cd7405e05578ad5b993fe11d29fdc344
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-14 11:21:59 +0100

    Started working on Contact Us Component and the Form

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -6,9 +6,27 @@ const ContactUs = () => {
       <h5 className="contact-us-text">Contact Us</h5>
       <form className="contact-form">
         <label htmlFor="name-input">Name</label>
-        <input type="text" name="" id="name-input" placeholder="Write your name"/>
+        <input
+          type="text"
+          name=""
+          id="name-input"
+          placeholder="Write your name"
+        />
         <label htmlFor="email-input">Email</label>
-        <input type="email" name="" id="email-input" placeholder="Enter your email"/>
+        <input
+          type="email"
+          name=""
+          id="email-input"
+          placeholder="Enter your email"
+        />
+        <label htmlFor="message-textarea">Message</label>
+        <textarea
+          name=""
+          id=""
+          cols="20"
+          rows="5"
+          placeholder="Write your message..."
+        ></textarea>
       </form>
     </div>
   );
