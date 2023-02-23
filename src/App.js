c673e2e5bda1b92d4052fdcf0ddc03267087de53
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-23 16:23:45 +0100

    Created the route for the tracking waiting component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import './App.css';
-import NavBar from './Components/NavBar/NavBar';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import HeroPage from './Components/HeroPage/HeroPage';
-import Steps from './Components/Steps/Steps';
-import Solution from './Components/Solution/Solution';
-import Footer from './Components/Footer/Footer';
-import Faqs from './Components/Faqs/Faqs';
-import Tracking from './Components/Tracking/Tracking'
+import "./App.css";
+import NavBar from "./Components/NavBar/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HeroPage from "./Components/HeroPage/HeroPage";
+import Steps from "./Components/Steps/Steps";
+import Solution from "./Components/Solution/Solution";
+import Footer from "./Components/Footer/Footer";
+import Faqs from "./Components/Faqs/Faqs";
+import Tracking from "./Components/Tracking/Tracking";
+import TrackingWaiting from "./Components/TrackingWaiting/TrackingWaiting";
 
 function App() {
   return (
     <div>
       <Router>
-      <NavBar/>
+        <NavBar />
         <Routes>
-           <Route exact path="/" element={<div>
-            <HeroPage/>
-            <Steps/>
-            <Solution/>
-           </div>}/>
-           <Route exact path="/contactus" element={
-              <Faqs/>
-           }>
-           </Route>
-           <Route exact path="/tracking" element={<Tracking/>}></Route>
+          <Route
+            exact
+            path="/"
+            element={
+              <div>
+                <HeroPage />
+                <Steps />
+                <Solution />
+              </div>
+            }
+          />
+          <Route exact path="/contactus" element={<Faqs />}></Route>
+          <Route exact path="/tracking" element={<Tracking />}></Route>
+          <Route
+            exact
+            path="/tracking-waiting"
+            element={<TrackingWaiting />}
+          ></Route>
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </div>
   );
