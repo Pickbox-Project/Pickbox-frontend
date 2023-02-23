5bb2529499a1e4462a2ffdfff4dfadc3ba5a7de4
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-23 16:22:00 +0100

    Started working on the tracking-waiting Component

--- src/Components/TrackingWaiting/TrackingWaiting.jsx
+++ src/Components/TrackingWaiting/TrackingWaiting.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import trackingWaiting from "../../Assets/images/trackingWaiting.png";
 import "./TrackingWaiting.css";
 
 const TrackingWaiting = () => {
-  return <div></div>;
+  return (
+    <div className="tracking-container">
+      <div className="tracking-text">
+        <h3>Live track your Order</h3>
+        <p>Please enter your tracking number</p>
+      </div>
+      <div className="tracking-input">
+        <AiOutlineSearch className="search-icon" />
+        <input type="search" placeholder="Search" />
+      </div>
+      <div className="tracking-result">
+        <img src={trackingWaiting} alt="Enter Tracking Number Icon" />
+      </div>
+    </div>
+  );
 };
 
 export default TrackingWaiting;
