790a57980b3276778ea17e9736d83d62d6d45d83
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-27 16:03:53 +0100

    Added Tracking Delivered Component route

--- src/Components/TrackingDelivered/TrackingDelivered.jsx
+++ src/Components/TrackingDelivered/TrackingDelivered.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const TrackingDelivered = () => {
-  return <div>TrackingDelivered</div>;
+  return (
+    <div className="tracking-container">
+      <div className="tracking-text">
+        <h3>Live track your Order</h3>
+        <p>Please enter your tracking number</p>
+      </div>
+      <div className="tracking-input">
+        <AiOutlineSearch className="search-icon" />
+        <input type="search" placeholder="8E368916IBD" />
+      </div>
+      <div className="tracking-result">
+        <img src={trackingTransit} alt="Waiting Tracking Icon" />
+      </div>
+      <button className="tracking-button" onClick={handleNavigate}>
+        More details
+      </button>
+    </div>
+  );
 };
 
 export default TrackingDelivered;
