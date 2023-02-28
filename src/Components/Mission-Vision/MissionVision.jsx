7ef04130f369d20fd055837259ba52aca588f8fd
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-28 17:50:30 +0100

    Started working on the Mission and Vision Component

--- src/Components/Mission-Vision/MissionVision.jsx
+++ src/Components/Mission-Vision/MissionVision.jsx
@@ -5,7 +5,7 @@ const MissionVision = () => {
   return (
     <div className="mission-vision-container">
       <div className="mission-container">
-        <div className="our-mission-container">
+        <div className="our-mission-text-container">
           <div className="our-text">
             <span>O</span>
             <span>U</span>
@@ -15,6 +15,30 @@ const MissionVision = () => {
             <span>M</span>
             <span>I</span>
             <span>S</span>
+            <span>S</span>
+            <span>I</span>
+            <span>O</span>
+            <span>N</span>
+          </div>
+        </div>
+        <h5>
+          To foster a global solution platform <br />
+          that makes it easier for individuals
+          <br />
+          and businesses to trade.
+        </h5>
+      </div>
+      {/* Vision Container */}
+      <div className="mission-container">
+        <div className="our-mission-text-container">
+          <div className="our-text">
+            <span>O</span>
+            <span>U</span>
+            <span>R</span>
+          </div>
+          <div className="mission-text">
+            <span>V</span>
+            <span>I</span>
             <span>S</span>
             <span>I</span>
             <span>O</span>
