ebc24acb1dc15e2dcaa8c73810f4e4a8a5da44ac
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-28 12:18:26 +0100

    Started working on the About Us component and started adding elements needed

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -8,16 +8,18 @@ const AboutUs = () => {
         <div className="about-box">
           <h3>ABOUT US</h3>
         </div>
-        <h5>
-          Pickbox is a cutting-edge technology enabled market-to-doorstep
-          logistics startup that was established in 2023 as an innovative means
-          to providing easy, convenient, and reliable package delivery service
-          from merchants in markets to their customers' doorsteps. Our app
-          eliminates the hassles of individuals (end-users) spending hours in
-          exhausting commutes, endless waiting for commercial drivers, and
-          paying unwarranted charges to storekeepers in major motor parks in
-          every city by providing door-step delivery as a premium alternative.
-        </h5>
+        <div className="about-text">
+          <h5>
+            Pickbox is a cutting-edge technology enabled market-to-doorstep
+            logistics startup that was established in 2023 as an innovative
+            means to providing easy, convenient, and reliable package delivery
+            service from merchants in markets to their customers' doorsteps. Our
+            app eliminates the hassles of individuals (end-users) spending hours
+            in exhausting commutes, endless waiting for commercial drivers, and
+            paying unwarranted charges to storekeepers in major motor parks in
+            every city by providing door-step delivery as a premium alternative.
+          </h5>
+        </div>
       </div>
     </div>
   );
