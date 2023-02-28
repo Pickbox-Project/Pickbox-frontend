b216ee1eff866f848fda10bc34b4fa385f0ce6bb
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-28 17:42:05 +0100

    Started working on the Mission and Vision Component

--- src/Components/Mission-Vision/MissionVision.jsx
+++ src/Components/Mission-Vision/MissionVision.jsx
@@ -2,7 +2,26 @@ import React from "react";
 import "./MissionVision.css";
 
 const MissionVision = () => {
-  return <div>MissionVision</div>;
+  return (
+    <div className="mission-vision-container">
+      <div className="mission-container">
+        <div className="our-text">
+          <span>O</span>
+          <span>U</span>
+          <span>R</span>
+        </div>
+        <div className="mission-text">
+          <span>M</span>
+          <span>I</span>
+          <span>S</span>
+          <span>S</span>
+          <span>I</span>
+          <span>O</span>
+          <span>N</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MissionVision;
