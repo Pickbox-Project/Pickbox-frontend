b283b3ee0dc078d2d585d47aad08c0a775cc039a
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-01 15:36:36 +0100

    Started styling Team component

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Team.css";
+import teamVectorRight from "../../Assets/images/team-vector-right.png";
+import teamVectorLeft from "../../Assets/images/team-vector-left.png";
 
 const Team = () => {
   return (
     <div className="team-container">
-      <div></div>
+      <img src={teamVectorRight} alt="" className="team-vector-right" />
+      <img src={teamVectorLeft} alt="" className="team-vector-left" />
     </div>
   );
 };
