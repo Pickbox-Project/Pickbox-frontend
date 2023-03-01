5bf620720f6099e2d60022e190dfcd0e4d779c17
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-01 15:59:27 +0100

    Started styling Team component

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Team.css";
 import teamVectorRight from "../../Assets/images/team-vector-right.png";
 import teamVectorLeft from "../../Assets/images/team-vector-left.png";
+import firstTeamMember from "../../Assets/images/Adediran Ifeoluwa.png";
 
 const Team = () => {
   return (
@@ -14,6 +15,13 @@ const Team = () => {
           The pickbox team is made up of very
           agile...............................................
         </h5>
+      </div>
+      <div className="team-gallery">
+        <img src={firstTeamMember} alt="" className="team-member-image" />
+        <div className="team-info-text">
+          <h5 className="team-member-name">Ifeoluwa Adediran</h5>
+          <h5 className="team-member-role">CEO</h5>
+        </div>
       </div>
     </div>
   );
