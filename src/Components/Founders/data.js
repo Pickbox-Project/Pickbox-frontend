34d8ee23ee2e114e2a5cc6b1d402cae39e00e74c
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-01 13:35:08 +0100

    Started styling the Founders Component

--- src/Components/Founders/data.js
+++ src/Components/Founders/data.js
@@ -1,5 +1,6 @@
 import founderImage from "../../Assets/images/Adediran Ifeoluwa.png";
 import coFounderImage from "../../Assets/images/Mina Georgewill.png";
+import defaultImage from "../../Assets/images/default-image.png";
 export const foundersDatas = [{
         id: 1,
         founderName: "Ifeoluwa Adediran",
@@ -10,6 +11,12 @@ export const foundersDatas = [{
         id: 2,
         founderName: "Mina Georgewill",
         founderImage: coFounderImage,
+        founderRole: "Co-founder",
+    },
+    {
+        id: 3,
+        founderName: "Charles Adeyemi",
+        founderImage: defaultImage,
         founderRole: "Co-founder",
     },
 ];
