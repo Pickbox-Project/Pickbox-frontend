30d3b326c5f05c279acdebb6efcfe0ce322a02c5
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-01 12:27:42 +0100

    Added Founders Component to About Component

--- src/Components/Founders/Founders.jsx
+++ src/Components/Founders/Founders.jsx
@@ -5,7 +5,11 @@ import vectorRight from "../../Assets/images/vector right.png";
 const Founders = () => {
   return (
     <div className="founders-container">
-      <img src={vectorRight} alt="Vector Right" />
+      <img
+        src={vectorRight}
+        alt="Vector Right"
+        className="founders-vector-right"
+      />
       <div className="founder-text">
         <h3>Meet The Founders</h3>
       </div>
