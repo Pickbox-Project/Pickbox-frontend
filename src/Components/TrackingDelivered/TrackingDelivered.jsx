b4d28ea96939a5f092e22e9feaea443260209953
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-10 15:15:40 +0100

    Fixed responsiveness issues on the tracking images

--- src/Components/TrackingDelivered/TrackingDelivered.jsx
+++ src/Components/TrackingDelivered/TrackingDelivered.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import trackingDelivered from "../../Assets/images/tracking-delivered.png";
+import trackingDeliveredMobile from "../../Assets/images/trackingDeliveredMobile.png";
 
 const TrackingDelivered = () => {
   const navigate = useNavigate();
@@ -19,7 +20,16 @@ const TrackingDelivered = () => {
         <input type="search" placeholder="8E368916IBD" />
       </div>
       <div className="tracking-result">
-        <img src={trackingDelivered} alt="Waiting Tracking Icon" />
+        <img
+          src={trackingDelivered}
+          className="trackingImage"
+          alt="Waiting Tracking Icon"
+        />
+        <img
+          src={trackingDeliveredMobile}
+          className="trackingMobile"
+          alt="Waiting Tracking Icon"
+        />
       </div>
       <button className="tracking-button" onClick={handleNavigate}>
         More details
