b9876923cdce7d68cb33c43d4984b88999d6a69c
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-10 14:47:54 +0100

    Started Designing the Tracking Error Component

--- src/Components/TrackingError/TrackingError.jsx
+++ src/Components/TrackingError/TrackingError.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import trackingError from "../../Assets/images/trackingError.png";
 
 const TrackingError = () => {
   return (
-    <div className="tracking-error-container">
-      <div></div>
+    <div className="tracking-container">
+      <div className="tracking-text">
+        <h3>Live track your Order</h3>
+        <p>Please enter your tracking number</p>
+      </div>
+      <div className="tracking-input">
+        <AiOutlineSearch className="search-icon" />
+        <input type="search" placeholder="8E368916IBD" />
+      </div>
+      <div className="tracking-result">
+        <img src={trackingError} alt="Waiting Tracking Icon" />
+      </div>
     </div>
   );
 };
