5e0a5ee2225710bfd5234f66cffbe7f15848c39a
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-22 15:01:21 +0100

    Started consuming the Tracking Endpoint

--- src/Components/Tracking/Tracking.jsx
+++ src/Components/Tracking/Tracking.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import "./Tracking.css";
+import { useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import entertrackingNumberImage from "../../Assets/images/enter-tracking number.png";
 import trackingMobile from "../../Assets/images/tracking-default-mobile.png";
@@ -7,12 +8,16 @@ import axios from "axios";
 
 const Tracking = () => {
   const searchRef = useRef();
+  const navigate = useNavigate();
   const getTrackingResult = async () => {
     const searchValue = searchRef.current.value;
     try {
       const getTrackingdata = await axios.get(
         `https://pickbox.azurewebsites.net/api/Tracking/View-TrackingInformation?trackingCode=${searchValue}`
       );
+      if (getTrackingdata.data.succeeded === false) {
+        navigate("/TrackingError");
+      }
       console.log(getTrackingdata);
     } catch (e) {
       console.error(e);
