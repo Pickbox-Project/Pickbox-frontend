9510d3a37b3c90d44448371b47c6d8e4f0444c77
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-25 12:08:55 +0100

    Fixed the preloader

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ function App() {
                     <img src={loaderImage} alt="loader" className="loader"/>
                 </div> :  <div><NavBar /></div>}
         </div>
-        {/* <NavBar /> */}
         <Routes>
           <Route
             exact
