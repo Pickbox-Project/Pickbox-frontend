36c483d12c8eb841ad3da18319b9f38a7a56acb7
Olagboye Seyi <akingbolaola@gmail.com>
2023-05-15 10:53:29 +0100

    Getting product Image from tracking Endpoint

--- src/Components/MoreDetails/MoreDetails.jsx
+++ src/Components/MoreDetails/MoreDetails.jsx
@@ -12,6 +12,7 @@ const MoreDetails = () => {
   const [bookStatus, setBookStatus] = useState("");
   const [receiverName, setReceiverName] = useState("");
   const [receiverDestination, setReceiverDestination] = useState("");
+  const [productImage, setProductImage] = useState(null);
   const [bookingPrice, setBookPrice] = useState(0);
   const storedTrackingID = localStorage.getItem("TrackingID");
   const getTrackingDetails = async () => {
